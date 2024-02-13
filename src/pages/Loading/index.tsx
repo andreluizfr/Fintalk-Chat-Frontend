f1d2a69fe52ea0fca8958a7d011a22541711f2ae
André Luiz <andre.luizfr2@gmail.com>
2024-02-13 13:35:28 -0300

    feat: adding theme to loading page

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,14 +1,18 @@
 import './styles.scss';
 
+import { StoreState } from '@store/redux/config';
+import { useSelector } from 'react-redux';
 import DotLoader from "react-spinners/DotLoader";
 
 export default function Loading(): JSX.Element {
 
+  const themeStore = useSelector((state: StoreState) => state.theme);
+
   return (
-    <main className='LoadingPage'>
+    <main className='LoadingPage' data-theme={themeStore.selectedTheme}>
       <DotLoader
         className='Loader'
-        color="rgb(229,9,20)"
+        color="#f3537e"
         size={70}
         aria-label="Loading Spinner"
         data-testid="loader"
